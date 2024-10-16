1db65dd45568c7192e52c870ac58dd20eacbcdf0
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-10-15 23:06:28 -0300

    Adding view extensions

---
+++ app/src/main/java/com/picpay/desafio/android/core/extensions/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.picpay.desafio.android.core.extensions
+
+import android.view.View
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
