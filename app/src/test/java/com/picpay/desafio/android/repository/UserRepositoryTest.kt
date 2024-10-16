a0726e5dc4991a91b894adfd02e3240acf1f550a
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-10-16 19:44:25 -0300

    refactoring tests

--- app/src/test/java/com/picpay/desafio/android/repository/UserRepositoryTest.kt
+++ app/src/test/java/com/picpay/desafio/android/repository/UserRepositoryTest.kt
@@ -1,27 +1,30 @@
 package com.picpay.desafio.android.repository
 
-import com.picpay.desafio.android.core.data.mapper.UserMapper.toDomain
-import com.picpay.desafio.android.core.data.repository.UserRepositoryImpl
-import com.picpay.desafio.android.core.data.service.PicPayService
 import com.picpay.desafio.android.core.common.Result
 import com.picpay.desafio.android.core.data.database.UserDao
+import com.picpay.desafio.android.core.data.repository.UserRepositoryImpl
+import com.picpay.desafio.android.core.data.service.PicPayService
 import com.picpay.desafio.android.core.domain.repository.UserRepository
+import com.picpay.desafio.android.fake.model.fakeUserEntityList
+import com.picpay.desafio.android.fake.model.fakeUserList
 import com.picpay.desafio.android.fake.model.fakeUserResponseList
+import io.mockk.Called
+import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.coVerifySequence
+import io.mockk.just
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class UserRepositoryTest {
 
-    private val service: PicPayService = mockk()
-    private val userDao: UserDao = mockk()
+    private val service = mockk<PicPayService>(relaxed = true)
+    private val userDao = mockk<UserDao>(relaxed = true)
 
     private lateinit var userRepository: UserRepository
 
@@ -31,26 +34,63 @@ class UserRepositoryTest {
     }
 
     @Test
-    fun `getUsers returns Success when service returns data`() = runTest {
-        val expectedUsers = fakeUserResponseList.map { it.toDomain() }
-        coEvery { service.getUsers() } returns fakeUserResponseList
+    fun `getUsers returns users from database when available`() = runTest {
+        coEvery { userDao.getAllUsers() } returns fakeUserEntityList
 
         val result = userRepository.getUsers()
 
+        coVerify(exactly = 1) { userDao.getAllUsers() }
+        coVerify { service wasNot Called }
         assertTrue(result is Result.Success)
-        assertEquals(expectedUsers, (result as Result.Success).data)
-        coVerify(exactly = 1) { service.getUsers() }
+        assertEquals(fakeUserList, (result as Result.Success).data)
     }
 
     @Test
-    fun `getUsers returns Error when service throws exception`() = runTest {
-        val exception = Exception("Network error")
+    fun `getUsers fetches from service when database is empty`() = runTest {
+        coEvery { userDao.getAllUsers() } returns emptyList()
+
+        coEvery { service.getUsers() } returns fakeUserResponseList
+        coEvery { userDao.clearUsers() } just Runs
+        coEvery { userDao.insertUsers(fakeUserEntityList) } just Runs
+
+        val result = userRepository.getUsers()
+
+        coVerifySequence {
+            userDao.getAllUsers()
+            service.getUsers()
+            userDao.clearUsers()
+            userDao.insertUsers(fakeUserEntityList)
+        }
+        assertTrue(result is Result.Success)
+        assertEquals(fakeUserList, (result as Result.Success).data)
+    }
+
+    @Test
+    fun `getUsers returns error when exception occurs in database`() = runTest {
+        val exception = Exception("Database Error")
+        coEvery { userDao.getAllUsers() } throws exception
+
+        val result = userRepository.getUsers()
+
+        coVerify { userDao.getAllUsers() }
+        coVerify { service wasNot Called }
+        assertTrue(result is Result.Error)
+        assertEquals(exception, (result as Result.Error).exception)
+    }
+
+    @Test
+    fun `getUsers returns error when exception occurs in service`() = runTest {
+        coEvery { userDao.getAllUsers() } returns emptyList()
+        val exception = Exception("Service Error")
         coEvery { service.getUsers() } throws exception
 
         val result = userRepository.getUsers()
 
+        coVerifySequence {
+            userDao.getAllUsers()
+            service.getUsers()
+        }
         assertTrue(result is Result.Error)
         assertEquals(exception, (result as Result.Error).exception)
-        coVerify(exactly = 1) { service.getUsers() }
     }
 }
