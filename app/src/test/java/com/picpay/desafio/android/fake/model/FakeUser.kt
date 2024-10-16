58923b0ffde735cc21c3de9413c708a4f43ed1ed
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2024-10-16 12:49:00 -0300

    Adding fake models for testing

---
+++ app/src/test/java/com/picpay/desafio/android/fake/model/FakeUser.kt
@@ -0,0 +1,16 @@
+package com.picpay.desafio.android.fake.model
+
+import com.picpay.desafio.android.core.domain.model.User
+
+val fakeUser = User(
+    id = 1,
+    image = "https://randomuser.me/api/portraits",
+    name = "Fake User",
+    username = "fakeuser"
+)
+
+val fakeUserList = listOf(
+    fakeUser,
+    fakeUser.copy(id = 2),
+    fakeUser.copy(id = 3),
+)
